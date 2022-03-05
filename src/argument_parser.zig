18f4183b13855706e3f074ae573ca76666874f7b
Diego Ugarte <diego.ugarte@outlook.com>
2022-03-05 20:24:02 +0900

    Update parser

--- src/argument_parser.zig
+++ src/argument_parser.zig
@@ -1,36 +1,31 @@
 const argparse = @import("argparse");
+const AppOption = argparse.AppOption;
 
 pub const ArgumentParser = argparse.ArgumentParser(.{
     .app_name = "entropy",
     .app_description = "Entropy calculator.",
     .app_version = .{ .major = 0, .minor = 1, .patch = 0 },
-}, &[_]argparse.AppOptionPositional{
-    .{
-        .option = .{
-            .name = "shannon",
-            .long = "--shannon",
-            .description = "Calculate shannon entropy",
-        },
+}, &.{
+    AppOption{
+        .name = "shannon",
+        .long = "--shannon",
+        .description = "Calculate shannon entropy",
     },
-    .{
-        .option = .{
-            .name = "file",
-            .short = "-f",
-            .long = "--file",
-            .metavar = "FILE",
-            .description = "Input file",
-            .takes = 1,
-            .conflicts_with = &.{"string"},
-        },
+    AppOption{
+        .name = "file",
+        .short = "-f",
+        .long = "--file",
+        .metavar = "FILE",
+        .description = "Input file",
+        .takes = 1,
+        .conflicts_with = &.{"string"},
     },
-    .{
-        .option = .{
-            .name = "string",
-            .short = "-s",
-            .long = "--string",
-            .metavar = "S",
-            .description = "Input string",
-            .takes = 1,
-        },
+    AppOption{
+        .name = "string",
+        .short = "-s",
+        .long = "--string",
+        .metavar = "S",
+        .description = "Input string",
+        .takes = 1,
     },
-});
+}, &.{});
